6f529b6c5d7d9aa40cb142f51ddc125ae13fb81a
idir <idirnezlioui4@gmail.com>
2024-04-03 14:04:50 +0200

    Update product.js

--- src/componenet/product.js
+++ src/componenet/product.js
@@ -1,14 +1,17 @@
+import { Link } from "react-router-dom";
 function Product(props) {
-    const {pro}=props
+    const {pro,showbutton}=props
     
     return(
         <>
-            <div className="card" style={{ width: "18rem" }} >
+            <div className="card" style={{ width: "18rem"}} >
                         <img src={pro.image} className="card-img-top" alt={pro.title}/>
                         <div className="card-body">
                             <h5 className="card-title">{pro.title}</h5>
                             <p className="card-text">{pro.description}</p>
-                            <button href="#" className="btn btn-primary">Detail</button>
+                            <p>Price :{pro.price}$</p>
+                             {showbutton && <Link
+                              className="btn btn-primary" to={`/product/${pro.id}`}>Detail</Link> }
                         </div>
             </div>
         </>
