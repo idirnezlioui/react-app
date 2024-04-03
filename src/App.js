fc910c9c10602680ec769cdad177ceec2fb4259b
idir <idirnezlioui4@gmail.com>
2024-04-03 14:05:03 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Carusal from "./componenet/Carusal";
 import ProductList from "./componenet/ProductList";
 import About from "./componenet/About";
 import {Routes,Route} from "react-router-dom"
+import ProductDetail from "./componenet/ProductDetail";
 function App() {
   return (
     <div className="App">
@@ -20,6 +21,8 @@ function App() {
             <About/>
           </>
         }>
+        </Route>
+        <Route path="/Product/:ProductId" element={<ProductDetail/>}>
 
         </Route>
       </Routes>
